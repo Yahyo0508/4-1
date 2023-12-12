7e7bad3720f3d18801e985ef51127aec8a18a477
Yahyo0508 <yahyo0508@gmail.com>
2023-12-12 22:04:17 +0300

    1-dars

---
+++ main.js
@@ -0,0 +1,7 @@
+let ism = prompt ("Ismingizni kiriting")
+let familiya = prompt ("Familiyangizni kiriting")
+let yosh = +prompt ("Yoshingizni kiriting")
+
+console.log(`Ism: ${ism}`)
+console.log(`Familiya: ${familiya}`)
+console.log(`Yosh: ${yosh}`)
